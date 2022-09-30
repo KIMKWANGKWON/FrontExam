9b1ee3d62a6472d1cd93f3f61d6d815435859017
KIMKWANGKWON <114123487+KIMKWANGKWON@users.noreply.github.com>
2022-09-30 12:48:55 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,53 @@
 boards = {
     title: new Array(),
     contents: new Array(),
-    days: new Array(),
-    insert: function () {
+    hits: new Array(),
+    days : new Array(),
+        insert: function () {
         boards.title.push(document.querySelector('#floatingInput').value);
         boards.contents.push(document.querySelector('#floatingPassword').value);
         document.querySelector('#floatingInput').value = '';
         document.querySelector('#floatingPassword').value = '';
-        let today = new Date();   
-
+        let today = new Date();
+        boards.days.push(today);
+        boards.hits.push(0);
         let year = today.getFullYear(); // 년도
         let month = today.getMonth() + 1;  // 월
         let date = today.getDate();  // 날짜
         let day = today.getDay();  // 요일        target = document.getElementById("board");
         var st1 = "";
         target = document.getElementById("board");
-        for(var i=0;i<boards.title.length;i++) {
-            st1 += ("<tr><td class='title'><a href=''>" + boards.title[i] + "</a></td><td class='name'>김광권</td><td class='date'>" + today.getFullYear() + "/" + (today.getMonth() + 1)+ "/" + today.getDate() + "</td><td class='hit'>0</td></tr>");
+        for (var i = 0; i < boards.title.length; i++) {
+            st1 += ("<tr><td class='title'><a href='javascript:view_contents(" + i + ")'>" + boards.title[i] + "</a></td><td class='name'>김광권</td><td class='date'>" + today.getFullYear() + "/" + (today.getMonth() + 1) + "/" + today.getDate() + "</td><td class='hit'>" + boards.hits[i] + "</td></tr>");
         }
         target.innerHTML = "<table class='sub_news' border='1' cellspacing='0' summary='게시판의 글제목 리스트'><caption>게시판 리스트</caption><colgroup><col><col width='110'><col width='100'><col width='80'></colgroup><thead><tr><th scope='col'>제목</th><th scope='col'>글쓴이</th><th scope='col'>날짜</th><th scope='col'>조회수</th></tr></thead><tbody>"
-        + st1 + '</tbody></table><input type="button" value="글쓰기" onclick="w_ing_on()">'
+            + st1 + '</tbody></table><input type="button" value="글쓰기" onclick="w_ing_on()">'
     }
+}
+function boards_reload() {
+    debugger;
+    if (boards.title.length === 0) { }
+    else {
+        var st1 = "";
+        var today;
+        target = document.getElementById("board");
+        for (var i = 0; i < boards.title.length; i++) {
+            today = boards.days[i]; 
+            st1 += ("<tr><td class='title'><a href='javascript:view_contents(" + i + ")'>" + boards.title[i] + "</a></td><td class='name'>김광권</td><td class='date'>" + today.getFullYear() + "/" + (today.getMonth() + 1) + "/" + today.getDate() + "</td><td class='hit'>" + boards.hits[i] + "</td></tr>");
+        }
+        target.innerHTML = "<table class='sub_news' border='1' cellspacing='0' summary='게시판의 글제목 리스트'><caption>게시판 리스트</caption><colgroup><col><col width='110'><col width='100'><col width='80'></colgroup><thead><tr><th scope='col'>제목</th><th scope='col'>글쓴이</th><th scope='col'>날짜</th><th scope='col'>조회수</th></tr></thead><tbody>"
+            + st1 + '</tbody></table><input type="button" value="글쓰기" onclick="w_ing_on()">'
+    }
+}
+function view_contents(index) {
+    debugger;
+    boards.hits[index]++;
+    displayOn("#w_contents");
+    var st1 =
+        '<h1>' + boards.title[index] + '</h1>' +
+        '<p class="lead">' + boards.contents[index] + '</p>';
+    target = document.getElementById("w_contents");
+    target.innerHTML = st1;
 }
 function w_ing_on() {
     displayOn("#w_ing");
